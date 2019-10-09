c4616873c7f8a2087f142186252308a4a8a0dd6b
Dmitry <koliasam@yandex.ru>
2019-10-09 14:20:06 +0300

    First commit to Lab two

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,15 +7,13 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         int[] arrayOne = {1,4,2,6,3};
-        int[] arrayTwo = {6,5,4,3,2,1,7};
+        int[] arrayOneTwo = {4,3,2,5,1,9};
+        int[] arrayTwo = {6,5,4,3,8,2,1,7};
 
-        System.out.println("Bubble, test 1 - " + Arrays.toString(bubbleSort(arrayOne))); // 6 4 3 2 1
-        System.out.println("Select, test 1 - " + Arrays.toString(selectionSort(arrayOne)));
-        System.out.println("Combine, test 1 - " + Arrays.toString(combineSort(arrayOne)) + "\n");
+        System.out.println("Bubble, test 1 - " + Arrays.toString(bubbleSort(arrayOne))); // 1 2 3 4 6
+        System.out.println("Select, test 1 - " + Arrays.toString(selectSort(arrayOneTwo))); // 1 2 3 4 5 9
+        System.out.println("Combine, test 1 - " + Arrays.toString(combineSort(arrayTwo)) + "\n"); // 1 2 3 4 5 6 7 8
 
-        System.out.println("Bubble, test 2 - " + Arrays.toString(bubbleSort(arrayTwo))); // 1 2 3 4 5 6 7
-        System.out.println("Selection, test 2 - " + Arrays.toString(selectionSort(arrayTwo)));
-        System.out.println("Combine, test 2 - " + Arrays.toString(combineSort(arrayTwo)));
     }
 
     private static int[] bubbleSort(int[] arr) {  // Сортировка методом Пузырька
@@ -32,17 +30,17 @@ public class Main {
    }
 
 
-   private static int[] selectionSort(int[] arr) {   // Сортировка выборкой
+   private static int[] selectSort(int[] arr) {   // Сортировка выборкой
         for (int i = 0; i < arr.length - 1; i++) {
-            int max = i;
+            int min = i;
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] > arr[max]) {
-                    max = i;
+                if (arr[j] < arr[min]) {
+                    min = j;
                 }
-                int swap = arr[max];
-                arr[max] = arr[i];
-                arr[i] = swap;
             }
+                int swap = arr[min];
+                arr[min] = arr[i];
+                arr[i] = swap;
         }
         return arr;
    }
@@ -55,10 +53,10 @@ public class Main {
                 lenArr = (int) (lenArr / 1.2473309);
             }
             for (int i = 0, j = lenArr; j < arr.length; i++, j++) {
-                if (arr[i] > arr[j]) {
+                if (arr[i] >= arr[j]) {
                     int swap = arr[i];
-                    arr[i] = arr[j + i];
-                    arr[j + i] = swap;
+                    arr[i] = arr[j];
+                    arr[j] = swap;
                 }
             }
         }
